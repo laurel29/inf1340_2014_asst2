1ac8c804bad799a53ada71efdc79b40396314e14
laurel29 <laurelrobinson@mail.utoronto.ca>
2014-11-10 21:15:14 -0500

    Took code from Yasmin's repository and added it - added docstrings - added test cases - changed visa parameters  from visa to (visa_code, visa_date)

--- papers.py
+++ papers.py
@@ -16,7 +16,6 @@ import datetime
 import json
 
 
-
 def decide(input_file, watchlist_file, countries_file):
     """
     Decides whether a traveller's entry into Kanadia should be accepted
@@ -27,27 +26,181 @@ def decide(input_file, watchlist_file, countries_file):
         an entry or transit visa is required, and whether there is currently a medical advisory
     :return: List of strings. Possible values of strings are: "Accept", "Reject", "Secondary", and "Quarantine"
     """
-    with open('example_entries.json', 'r') as input_file:
+    results = list()
+
+    with open(input_file, 'r') as input_file:
         input_file_contents = input_file.read()
         entries_contents = json.loads(input_file_contents)
 
-    with open('watchlist.json', 'r') as watchlist_file:
+    with open(watchlist_file, 'r') as watchlist_file:
         watchlist_file_contents = watchlist_file.read()
         watchlist_contents = json.loads(watchlist_file_contents)
 
-    with open('countries.json', 'r') as countries_file:
+    with open(countries_file, 'r') as countries_file:
         countries_file_contents = countries_file.read()
         countries_contents = json.loads(countries_file_contents)
 
-    for e in entries_contents:
-        for e_value in e.values():
-            for w in watchlist_contents:
-                for w_value in w.values():
-                    if e_value == w_value:
-                        return "Secondary"
+    for entry in entries_contents:
+        #Check for quarantine
+        #Check if person on the watchlist
+        #Check if person returning and home country is KAN
+        #Check for visas
 
-    #else:
-        return ["Reject"]
+        entry_status = "Quarantine"
+
+        entry_first_name = entry["first_name"]
+        entry_last_name = entry["last_name"]
+        entry_passport = entry["passport"]
+        entry_reason = entry["entry_reason"]
+
+        entry_home = dict()
+        entry_from = dict()
+        entry_via = dict()
+        entry_visa = dict()
+
+        if "home" in entry:
+            entry_home = entry["home"]
+
+        if "from" in entry:
+            entry_from = entry["from"]
+
+        if "via" in entry:
+            entry_via = entry["via"]
+
+        if "visa" in entry:
+            entry_visa = entry["visa"]
+
+        if not entry_first_name or not entry_last_name or not entry_passport or not entry_home or not entry_from or not\
+                entry_reason:
+            if entry_via:
+                if check_medical_advisory(entry_via["country"], countries_contents):
+                    #this condition checks if the country is on the medical advisory list
+                    entry_status = "Quarantine"
+                    #if the country is on the medical list then it will send the entry to Quarantine
+                    results.append(entry_status)
+                    continue
+                    #continues to the next loop
+
+            if entry_home:
+                if check_medical_advisory(entry_from["country"], countries_contents):
+                    entry_status = "Quarantine"
+                    results.append(entry_status)
+                    continue
+
+            entry_status = "Reject"
+            results.append(entry_status)
+            continue
+        elif not valid_passport_format(entry_passport):
+            entry_status = "Reject"
+            results.append(entry_status)
+            continue
+        else:
+            if entry_home["country"] == "KAN" and entry_reason == "returning":
+                if entry_via:
+                    #Checks via country against countries with medical advisory
+                    if check_medical_advisory(entry_via["country"], countries_contents):
+                        #if the via country has a medical advisory the entry is sent to Quarantine
+                        entry_status = "Quarantine"
+                        results.append(entry_status)
+                        continue
+
+                if check_medical_advisory(entry_from["country"], countries_contents):
+                    entry_status = "Quarantine"
+                    results.append(entry_status)
+                    continue
+
+                watch_status = check_watch_list(entry_first_name, entry_last_name, entry_passport, watchlist_contents)
+                #Checks if entry is on the watchlist, returns Reject or Secondary if on list, Accept if not on list
+                if watch_status == "Reject":
+                    entry_status = "Reject"
+                    results.append(entry_status)
+                    continue
+                elif watch_status == "Secondary":
+                    entry_status = "Secondary"
+                    results.append(entry_status)
+                    continue
+                else:
+                    entry_status = "Accept"
+                    results.append(entry_status)
+                    continue
+            else:
+                #Checks if the via country has a medical advisory
+                if entry_via:
+                    if check_medical_advisory(entry_via["country"], countries_contents):
+                        entry_status = "Quarantine"
+                        results.append(entry_status)
+                        continue
+                #Checks if the country traveled from has a medical advisory
+                if check_medical_advisory(entry_from["country"], countries_contents):
+                    entry_status = "Quarantine"
+                    results.append(entry_status)
+                    continue
+                #Checks if the country traveled from requires a visa for entry
+                if check_visa_required(entry_from["country"], countries_contents, entry_reason):
+                    if not entry_visa:
+                        entry_status = "Reject"
+                        results.append(entry_status)
+                        continue
+                    elif not valid_visa(entry_visa):
+                        entry_status = "Reject"
+                        results.append(entry_status)
+                        continue
+
+                #Checks status on watchlist, returns either Secondary or Reject if on watchlist
+                #Returns accept if entry not on watch list
+                watch_status = check_watch_list(entry_first_name, entry_last_name, entry_passport, watchlist_contents)
+                if watch_status == "Secondary":
+                    entry_status = "Secondary"
+                    results.append(entry_status)
+                    continue
+                elif watch_status == "Reject":
+                    entry_status = "Reject"
+                    results.append(entry_status)
+                    continue
+                else:
+                    entry_status = "Accept"
+                    results.append(entry_status)
+                    continue
+    return results
+
+
+#Used to check if a country has a medical advisory or not
+def check_medical_advisory(country, country_list):
+    #If country has medical advisory, returns True
+    if country_list[country]["medical_advisory"]:
+        return True
+    #If not, return False
+    else:
+        return False
+
+
+def check_watch_list(first_name, last_name, passport, watchlist):
+    # Check person against every record in the watchlist return Reject or Secondary if found
+    # Return Accept if not in list
+    for item in watchlist:
+        if first_name == item["first_name"] and last_name == item["last_name"] and passport == item["passport"]:
+            return "Reject"
+        elif first_name == item["first_name"] or last_name == item["last_name"] or passport == item["passport"]:
+            return "Secondary"
+        else:
+            continue
+    else:
+        return "Accept"
+
+
+def check_visa_required(country, country_list, entry_reason):
+    # Check if visa is required for country with appropriate reason
+    # Return True or False
+    if entry_reason == "visit":
+        if country_list[country]["visitor_visa_required"] == "1":
+            return True
+        else:
+            return False
+    elif entry_reason == "transit":
+        if country_list[country]["transit_visa_required"] == "1":
+            return True
+        else:
+            return False
 
 
 def valid_passport_format(passport_number):
@@ -59,6 +212,36 @@ def valid_passport_format(passport_number):
     passport_format = re.compile('.{5}-.{5}-.{5}-.{5}-.{5}')
 
     if passport_format.match(passport_number):
+        return True
+    else:
+        return False
+
+
+def valid_visa(visa_number, visa_date):
+    """
+    Checks whether a visa number is 2 sets of five alpha-number characters separated by dashes
+    :rtype : object
+    :param visa_number: alpha-numeric string
+    :return: Boolean; True if the format is valid, False otherwise
+    """
+
+    valid_format = False
+    valid_date = False
+
+    visa_format = re.compile('.{5}-.{5}')
+
+    if visa_format.match(visa_number):
+        valid_format = True
+
+    visa_years = datetime.datetime.now() - datetime.datetime.strptime(visa_date, "%Y-%m-%d")
+    visa_years_difference = visa_years.days / 365
+
+    if visa_years_difference < 2:
+        valid_date = True
+        #this if condition checks if the visa validity is less than two years
+
+    if valid_date and valid_format:
+        #return True only if visa has a valid format and is not over two years old
         return True
     else:
         return False
--- test_papers.py
+++ test_papers.py
@@ -13,6 +13,9 @@ __status__ = "Prototype"
 # imports one per line
 import pytest
 from papers import decide
+from papers import valid_passport_format
+from papers import valid_date_format
+from papers import valid_visa
 
 
 def test_basic():
@@ -25,5 +28,20 @@ def test_files():
     with pytest.raises(FileNotFoundError):
         decide("test_returning_citizen.json", "", "countries.json")
 
-# add functions for other tests
 
+def test_valid_passport_format():
+    assert valid_passport_format("RHDP2-TY5E4-S4BGO-VSKW7-ID3TX") is True
+    assert valid_passport_format("T3SDR-T7JU8-E4TFH-BV34J-NI34") is False
+    assert valid_passport_format("RE45-HY78T-3DRF4-OP113-DOJ41") is False
+
+
+def test_valid_date_format():
+    assert valid_date_format("2014-10-03") is True
+    assert valid_date_format("09-12-1987") is False
+    assert valid_date_format("111-03-21") is False
+
+
+def test_valid_visa():
+    assert valid_visa("LY71X-APFE8", "2013-04-03") is True
+    assert valid_visa("I5K2J-HI90", "2013-09-11") is False
+    assert valid_visa("T6HY7-ETG67", "2007-12-02") is False
